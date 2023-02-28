a56ef35d2ff598ac50150921f687d16e3bf38066
Sagynaliev Taalaibek <sagynalievv.t@gmail.com>
2023-02-28 10:17:43 +0600

    ne w changes

--- MvcMovie_Taalaibek/Controllers/HelloWorldController.cs
+++ MvcMovie_Taalaibek/Controllers/HelloWorldController.cs
@@ -1,23 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using System.Text.Encodings.Web;
 
-namespace MvcMovie_Taalaibek.Controllers
-{
-    public class HelloWorldController : Controller
-    {
-        // GET: /HelloWorld/
+//namespace MvcMovie_Taalaibek.Controllers
+//{
+//    public class HelloWorldController : Controller
+//    {
+//        // GET: /HelloWorld/
 
-        public string Index()
-        {
-            return "This is my default action...";
-        }
+//        public string Index()
+//        {
+//            return "This is my default action...";
+//        }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
+//        // 
+//        // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int numTimes = 1)
-        {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-        }
-    }
-}
+//        public string Welcome(string name, int numTimes = 1)
+//        {
+//            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+//        }
+//    }
+//}

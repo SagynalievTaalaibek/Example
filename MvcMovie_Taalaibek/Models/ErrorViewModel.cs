5f53f45cf2ac8937cc4f749c45fae322d305a415
Sagynaliev Taalaibek <sagynalievv.t@gmail.com>
2023-02-28 09:50:22 +0600

    first commit contorller added

---
+++ MvcMovie_Taalaibek/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcMovie_Taalaibek.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
